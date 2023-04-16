e038c305abaad919aba57bcebb81d895522bafdd
DarkCoder <115655084+CoderBoyCodes@users.noreply.github.com>
2023-04-16 11:58:18 +0300

    Fixed issue 1 and revamped completely

--- SiteValidCheck.js
+++ SiteValidCheck.js
@@ -1,23 +1,6 @@
-alert("By clicking 'OK', you accept that we will go throught your full URL. Are you sure? (Reset if no)");
-let check = 0
-let index = []
-let error = 0
-index.push("/");
-index.push(".");
-index.push("https://");
-console.log("Ready to check");
-console.log("-----------------------------");
-while (check != 3) {
-    if (location.href.includes(index[check])) {
-        check++
-    } else if (!location.href.includes(index[check])) {
-        check++
-        error++
-    }
+alert("Please click 'OK' to proceed");
+if (location.href.includes("/") && location.href.includes(".") && location.href.includes("https://")) {
+    alert("Success! The domain is safe!");
+} else {
+    alert("Fail! The domain isn't safe!");
 }
-if (error > 0) {
-    alert("Failed! Your domain is suspicious..!");
-} else if (error = 0) {
-    alert("Success! Your domain is verified!");
-}
-console.log("C" + check + " - E" + error);
