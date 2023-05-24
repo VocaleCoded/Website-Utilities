489b292eff778c9b324a023ec3447e328ee22191
CoderBoyCodes <115655084+CoderBoyCodes@users.noreply.github.com>
2023-05-24 21:16:04 +0300

    Now checking for domain endpoints and validity

--- SiteValidCheck.js
+++ SiteValidCheck.js
@@ -1,6 +1,11 @@
 alert("Please click 'OK' to proceed");
-if (location.href.includes("/") && location.href.includes(".") && location.href.includes("https://")) {
-    alert("Success! The domain is safe!");
+if (location.href.includes('/') && location.href.includes(".") && location.href.includes("https://")) {
+    alert("Domain is vaild, proceed to check for malware");
+    if (!location.href.includes('.com') || !location.href.includes('.org' || !location.href.includes('.edu')) {
+        alert('The domain is uses a custom endpoint (."e.g. endpoint"), please be careful exploring it');
+    } else {
+        alert('The domain is considered safe, but still take care while exploring');
+    }
 } else {
-    alert("Fail! The domain isn't safe!");
+    alert("Domain is invalid, please do NOT continue exploring");
 }
