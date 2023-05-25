dd4a40e1eaaf3fad95d245d653360defd65197ac
CoderBoyCodes <115655084+CoderBoyCodes@users.noreply.github.com>
2023-05-25 10:04:06 +0300

    Added more accessibilty

--- TemporarySiteConfig.js
+++ TemporarySiteConfig.js
@@ -1,8 +1,18 @@
-(function () {
-var script=document.createElement('script');
-script.src='https://x-ray-goggles.mouse.org/webxray.js';
-script.className = 'webxray';
-script.setAttribute('data-lang','en-US');
-script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');
-document.body.appendChild(script);}()
-alert('Launched script (scripts by CoderBoyCodes on Github)'))
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://x-ray-goggles.mouse.org/webxray.js';
+  script.className = 'webxray';
+  script.setAttribute('data-lang', 'en-US');
+  script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
+  document.body.appendChild(script);
+
+  var timeout = setTimeout(function() {
+    alert('Failed to launch the script within 6 seconds.');
+  }, 6000);
+
+  script.onload = function() {
+    clearTimeout(timeout);
+    alert('Launched script (scripts by CoderBoyCodes on Github)');
+  };
+})();
+
