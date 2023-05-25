bcba4cb971672fc77b4c98be0b099bfb68576999
CoderBoyCodes <115655084+CoderBoyCodes@users.noreply.github.com>
2023-05-25 10:00:27 +0300

    More accurate, can be used as a bookmark

--- SiteValidCheck.js
+++ SiteValidCheck.js
@@ -1,11 +1,37 @@
-alert("Please click 'OK' to proceed");
-if (location.href.includes('/') && location.href.includes(".") && location.href.includes("https://")) {
-    alert("Domain is vaild, proceed to check for malware");
-    if (!location.href.includes('.com') || !location.href.includes('.org' || !location.href.includes('.edu')) {
-        alert('The domain is uses a custom endpoint (."e.g. endpoint"), please be careful exploring it');
-    } else {
-        alert('The domain is considered safe, but still take care while exploring');
+javascript:(function() {
+    alert("Please click 'OK' to proceed");
+
+    function getDomain(url) {
+        const parser = document.createElement('a');
+        parser.href = url;
+        return parser.hostname;
     }
-} else {
-    alert("Domain is invalid, please do NOT continue exploring");
-}
+
+    function isValidDomain(domain) {
+        const domainRegex = /^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+        return domainRegex.test(domain);
+    }
+
+    function hasMaliciousPattern(url) {
+        const maliciousPatterns = ['malware', 'phishing', 'attack'];
+        return maliciousPatterns.some(pattern => url.includes(pattern));
+    }
+
+    const currentDomain = getDomain(location.href);
+
+    if (isValidDomain(currentDomain)) {
+        alert("Domain is valid, proceed to check for malware");
+
+        if (!currentDomain.includes('.com') && !currentDomain.includes('.org') && !currentDomain.includes('.edu')) {
+            alert('The domain uses a custom endpoint (e.g., endpoint), please be careful exploring it');
+        } else {
+            alert('The domain is considered safe, but still take care while exploring');
+        }
+
+        if (hasMaliciousPattern(currentDomain)) {
+            alert('The domain contains a potential malicious pattern. Exercise caution while exploring.');
+        }
+    } else {
+        alert("Domain is invalid, please do NOT continue exploring");
+    }
+})();
