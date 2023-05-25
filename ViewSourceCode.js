0206c938e0e7c46b30b21d6b006d3556333bd0e9
CoderBoyCodes <115655084+CoderBoyCodes@users.noreply.github.com>
2023-05-25 23:52:44 +0300

    Made script yet more designful

--- ViewSourceCode.js
+++ ViewSourceCode.js
@@ -1,6 +1,16 @@
-(function() {
+javascript:(function() {
   var sourceCode = document.documentElement.innerHTML;
   var encodedSourceCode = encodeURIComponent(sourceCode);
   var newTab = window.open();
-  newTab.document.write('<pre>' + decodeURIComponent(encodedSourceCode).replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</pre>');
+  newTab.document.write(
+    '<html><head><title>Source Code</title>' +
+    '<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">' +
+    '<style>' +
+    '  body { background-color: #f5f5f5; }' +
+    '  pre { background-color: #fff; padding: 20px; font-family: "Fira Code", monospace; font-size: 14px; line-height: 1.5; color: #333; overflow-x: auto; }' +
+    '</style></head><body>' +
+    '<pre>' + decodeURIComponent(encodedSourceCode).replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</pre>' +
+    '</body></html>'
+  );
+  newTab.document.close();
 })();
