26f10fea9a81206eb0f0549cc6e10dd1ab5d6db5
CoderBoyCodes <115655084+CoderBoyCodes@users.noreply.github.com>
2023-07-16 12:53:34 +0300

    Create SiteWordCount.js

---
+++ SiteWordCount.js
@@ -0,0 +1,32 @@
+javascript:(function() {
+  var content = document.body.innerText;
+  var words = content.trim().split(/\s+/);
+  var wordCount = words.length;
+
+  const title = 'Word Count Tool';
+  const description = 'Word count of the main content: ' + wordCount + ' (Click to dismiss)';
+
+  const messageBox = document.createElement('div');
+
+  const messageContent = document.createElement('div');
+  messageContent.innerHTML = `<h2 style="margin: 0 0 5px; color: #FFFFFF; word-wrap: break-word;">${title}</h2><p style="margin: 0; color: #FFFFFF; word-wrap: break-word;">${description}</p>`;
+
+  messageBox.appendChild(messageContent);
+  document.body.appendChild(messageBox);
+
+  messageBox.style.position = 'fixed';
+  messageBox.style.top = '0';
+  messageBox.style.left = '0';
+  messageBox.style.width = '100%';
+  messageBox.style.backgroundColor = '#007AFF';
+  messageBox.style.padding = '10px 15px';
+  messageBox.style.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.2)';
+  messageBox.style.zIndex = '9999';
+  messageBox.style.fontFamily = 'Helvetica, Arial, sans-serif';
+  messageBox.style.fontSize = '16px';
+  messageBox.style.cursor = 'pointer';
+
+  messageBox.addEventListener('click', function() {
+    document.body.removeChild(messageBox);
+  });
+})();
